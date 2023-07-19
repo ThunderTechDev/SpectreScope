b6324e771638da438fb75c130dca9bd245516606
Sergio González <thundertechdev@icloud.com>
2023-07-19 23:02:39 +0200

    Initial Commit

---
+++ SpectreScope/SpectreScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpectreScopeApp.swift
+//  SpectreScope
+//
+//  Created by Sergio Gonzalez Cristobal on 19/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SpectreScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
