5e0c75371fada595477c6bd93170aa660385846d
Sergio González <thundertechdev@icloud.com>
2023-07-31 23:33:25 +0200

    Animating UI for looks like sonar

--- SpectreScope/Views/RadarScene.swift
+++ SpectreScope/Views/RadarScene.swift
@@ -45,20 +45,57 @@ class CompassHeading: NSObject, CLLocationManagerDelegate {
 class RadarScene: SKScene {
     let sprite = SKSpriteNode()
     let compassHeading = CompassHeading()
+    var perturbation: SKEmitterNode?
     
     override func didMove(to view: SKView) {
-        let texture = SKTexture(imageNamed: "Radar")
-        sprite.texture = texture
+        // Cargar las imágenes de la animación del radar
+        let radarTextures = [
+            SKTexture(imageNamed: "RadarAnimation01.png"),
+            SKTexture(imageNamed: "RadarAnimation02.png"),
+            SKTexture(imageNamed: "RadarAnimation03.png"),
+            SKTexture(imageNamed: "RadarAnimation04.png"),
+            SKTexture(imageNamed: "RadarAnimation05.png")
+        ]
+        
+        // Crear una acción de animación
+        let radarAnimation = SKAction.animate(with: radarTextures, timePerFrame: 0.02)
+        
+        // Crear una acción para mostrar la textura final
+        let finalTexture = SKAction.setTexture(SKTexture(imageNamed: "Radar.png"))
+        
+        // Crear acciones para hacer desaparecer y aparecer la perturbación
+        let fadeOut = SKAction.fadeOut(withDuration: 0.1)
+        let fadeIn = SKAction.fadeIn(withDuration: 0.1)
+        let hidePerturbation = SKAction.run { [weak self] in self?.perturbation?.run(fadeOut) }
+        let showPerturbation = SKAction.run { [weak self] in self?.perturbation?.run(fadeIn) }
+        
+        // Crear una acción de espera
+        let wait = SKAction.wait(forDuration: 1.0) // Esperar 1 segundo
+        
+        // Crear una secuencia de acciones: animación, mostrar textura final, ocultar perturbación, esperar, mostrar perturbación
+        let sequence = SKAction.sequence([showPerturbation, radarAnimation, hidePerturbation, finalTexture, wait])
+        
+        // Crear una acción que repita la secuencia indefinidamente
+        let repeatAnimation = SKAction.repeatForever(sequence)
+        
+        // Aplicar la acción al sprite
+        sprite.run(repeatAnimation)
+        
         sprite.size = CGSize(width: 400, height: 400)
         sprite.position = CGPoint(x: 0, y: 0)
         addChild(sprite)
         
         if let perturbation = SKEmitterNode(fileNamed: "Perturbation") {
+            self.perturbation = perturbation
             perturbation.position = CGPoint(x: 0, y: 100) // Inicia la perturbación en el borde del radar
             addChild(perturbation)
             compassHeading.perturbation = perturbation // Asignar la perturbación al manejador de la brújula
         }
     }
+
+
+
+
     
     override func update(_ currentTime: TimeInterval) {
         // Updating the Scene
