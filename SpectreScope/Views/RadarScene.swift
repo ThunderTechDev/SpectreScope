fa5693c82cc79f597be0a254da5e3e0b0fa60c49
Sergio González <thundertechdev@icloud.com>
2023-08-01 23:56:06 +0200

    Adding Sound to radar animation

--- SpectreScope/Views/RadarScene.swift
+++ SpectreScope/Views/RadarScene.swift
@@ -8,6 +8,7 @@
 import Foundation
 import CoreLocation
 import SpriteKit
+import AVFoundation
 
 class CompassHeading: NSObject, CLLocationManagerDelegate {
     var heading: Double = 0.0
@@ -46,7 +47,8 @@ class RadarScene: SKScene {
     let sprite = SKSpriteNode()
     let compassHeading = CompassHeading()
     var perturbation: SKEmitterNode?
-    
+    var audioPlayer: AVAudioPlayer?
+
     override func didMove(to view: SKView) {
         // Cargar las imágenes de la animación del radar
         let radarTextures = [
@@ -62,38 +64,50 @@ class RadarScene: SKScene {
         
         // Crear una acción para mostrar la textura final
         let finalTexture = SKAction.setTexture(SKTexture(imageNamed: "Radar.png"))
-        
+
         // Crear acciones para hacer desaparecer y aparecer la perturbación
-        let fadeOut = SKAction.fadeOut(withDuration: 0.1)
-        let fadeIn = SKAction.fadeIn(withDuration: 0.1)
+        let fadeOut = SKAction.fadeOut(withDuration: 0.2)
+        let fadeIn = SKAction.fadeIn(withDuration: 0.2)
         let hidePerturbation = SKAction.run { [weak self] in self?.perturbation?.run(fadeOut) }
         let showPerturbation = SKAction.run { [weak self] in self?.perturbation?.run(fadeIn) }
-        
+
         // Crear una acción de espera
         let wait = SKAction.wait(forDuration: 1.0) // Esperar 1 segundo
-        
+
+        // Crear una acción para reproducir el sonido
+        let playSound = SKAction.run { [weak self] in
+            self?.audioPlayer?.volume = 0.04
+            self?.audioPlayer?.play()
+        }
+
         // Crear una secuencia de acciones: animación, mostrar textura final, ocultar perturbación, esperar, mostrar perturbación
-        let sequence = SKAction.sequence([showPerturbation, radarAnimation, hidePerturbation, finalTexture, wait])
-        
+        let sequence = SKAction.sequence([showPerturbation, radarAnimation, hidePerturbation, playSound, finalTexture, wait])
+
         // Crear una acción que repita la secuencia indefinidamente
         let repeatAnimation = SKAction.repeatForever(sequence)
-        
+
         // Aplicar la acción al sprite
         sprite.run(repeatAnimation)
-        
+
         sprite.size = CGSize(width: 400, height: 400)
         sprite.position = CGPoint(x: 0, y: 0)
         addChild(sprite)
-        
+
         if let perturbation = SKEmitterNode(fileNamed: "Perturbation") {
             self.perturbation = perturbation
             perturbation.position = CGPoint(x: 0, y: 100) // Inicia la perturbación en el borde del radar
             addChild(perturbation)
             compassHeading.perturbation = perturbation // Asignar la perturbación al manejador de la brújula
         }
+
+        if let url = Bundle.main.url(forResource: "Tap", withExtension: "wav") {
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: url)
+            } catch {
+                print("No se pudo cargar el archivo de sonido.")
+            }
+        }
     }
-
-
 
 
     
